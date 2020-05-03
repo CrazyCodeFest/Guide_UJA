c780dbdca1421df2b4318a73424f58a9ed755e54
Adrián Arboledas <adrianarboledas5@gmail.com>
2020-05-04 00:28:52 +0200

    Implementacion titulaciones

---
+++ Guide /GuideUJA_UI/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  GuideUJA_UI
+//
+//  Created by Adrian Arboledas Fernandez on 03/05/2020.
+//  Copyright © 2020 Adrian Arboledas Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
---
+++ Guide /GuideUJA_UI/SecondTableView.swift
@@ -0,0 +1,9 @@
+//
+//  SecondTableView.swift
+//  GuideUJA_UI
+//
+//  Created by Adrian Arboledas Fernandez on 03/05/2020.
+//  Copyright © 2020 Adrian Arboledas Fernandez. All rights reserved.
+//
+
+import Foundation
