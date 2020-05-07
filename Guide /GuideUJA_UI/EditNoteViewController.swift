e1eb5c81a57345fe8068f95f96b46b6a6b3ab02c
Adrián Arboledas <adrianarboledas5@gmail.com>
2020-05-08 00:32:08 +0200

    Notas Fucnionando 100%, pero sin comentarios

---
+++ Guide /GuideUJA_UI/EditNoteViewController.swift
@@ -0,0 +1,78 @@
+//
+//  EditNoteViewController.swift
+//  GuideUJA_UI
+//
+//  Created by Adrian Arboledas Fernandez on 07/05/2020.
+//  Copyright © 2020 Adrian Arboledas Fernandez. All rights reserved.
+//
+
+import UIKit
+
+
+protocol EditNoteDelegate {
+    
+    func updateNote(updatedTitle: String, updatedBody: String)
+}
+
+
+class EditNoteViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    var editNoteDelegate: EditNoteDelegate?
+    var noteBody: String!
+    
+    
+    
+    @IBAction func doneButtonTap(_ sender: Any) {
+        
+        self.bodyTextView.resignFirstResponder()
+        
+        self.doneButton.isEnabled = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        super.viewDidLoad()
+        self.bodyTextView.delegate = self
+        self.bodyTextView.text = noteBody
+        self.bodyTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if self.editNoteDelegate != nil {
+            
+            self.editNoteDelegate?.updateNote(updatedTitle: self.getNotesTitle(), updatedBody: self.bodyTextView.text)
+            
+                    }
+    }
+    
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        self.doneButton.isEnabled = true
+    }
+    
+
+    func getNotesTitle() -> String  {
+        
+        let components = self.bodyTextView.text.components(separatedBy: "\n")
+        
+        for components in components {
+            if components.trimmingCharacters(in: CharacterSet.whitespaces).count > 0 {
+                return components
+            }
+        }
+        return self.navigationItem.title ?? ""
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
