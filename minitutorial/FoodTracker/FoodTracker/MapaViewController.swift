b7a0cc71862dc352d306220d81af8d1d04072029
Javi Aranda <j.arandaizquierdo@gmail.com>
2020-05-11 00:23:15 +0200

    Agregadas las variables para localizacion y Delegate del MapViewCrontroller

--- minitutorial/FoodTracker/FoodTracker/MapaViewController.swift
+++ minitutorial/FoodTracker/FoodTracker/MapaViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
 class MapaViewController: UIViewController {
 
+    @IBOutlet weak var MapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-     
+        //Damos una localizacion con la longitud y latitud
+        let localizacion = CLLocation(latitude: 37.7874, longitude: -3.7769)
+        //La distancia en radio que queremos visualizar
+        let radio: CLLocationDistance = 2000.0
+        //Creamos una region con los datos anteriores
+        let region = MKCoordinateRegionMakeWithDistance(localizacion.coordinate, radio, radio)
+        
+        //A nuestro MapView le damos esa region
+        MapView.setRegion(region, animated: true)
+        
+        MapView.delegate = self
+    }
+}
+
+extension MapaViewController: MKMapViewDelegate{
+    
+    func mapViewWillStartLoadingMap(_ mapView: MKMapView) {
+        print("renderizando....")
     }
 }
